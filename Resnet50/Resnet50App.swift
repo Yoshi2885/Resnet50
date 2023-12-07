ffe23e151b0621761e3cb7602b2e52af7fdb4760
Yoshi2885 <yoshitaka_tanaka_aa@mail.toyota.co.jp>
2023-12-07 11:34:04 +0900

    Initial Commit

---
+++ Resnet50/Resnet50App.swift
@@ -0,0 +1,17 @@
+//
+//  Resnet50App.swift
+//  Resnet50
+//
+//  Created by yoshitaka on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct Resnet50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
